d1f7a06208132fe1624c6c8224f1cacf0ab68bb0
spazidevps <157840094+spazidevps@users.noreply.github.com>
2026-04-23 17:05:01 -0600

    Update g_chicote.py

--- g_chicote.py
+++ g_chicote.py
@@ -1,19 +1,15 @@
 def asignar_chicote(altura):
-    if 1.59 <= altura <= 2.16:
-        return 2.7
-    elif 2.17 <= altura <= 2.65:
-        return 3.0
-    elif 2.66 <= altura <= 3.0:
-        return 3.4
-    elif 3.1 <= altura <= 3.31:
-        return 3.8
-    elif 3.39 <= altura <= 3.62:
-        return 4.2
-    elif 3.8 <= altura <= 4.0:
-        return 4.6
-    elif 4.9 <= altura <= 5.32:  # Corregido basado en la suposición de un error tipográfico
-        return 4.8
-    else:
-        return "No disponible para la altura ingresada" 
-    
-        
+    """
+    Calcula el chicote como:
+    altura + 0.40 m
+
+    Retorna un float redondeado a 2 decimales.
+    """
+
+    if altura is None or altura <= 0:
+        return "Altura no válida"
+
+    chicote = altura + 0.40
+
+    # return round(chicote, 2)
+    return float(f"{chicote:.2f}")
